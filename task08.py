1f65ecb57a33381713ef831e3b87022c6621f9fd
Jasur <uktamovjasur137@gmail.com>
2026-04-17 09:49:37 -0700

    done

---
+++ task08.py
@@ -0,0 +1,17 @@
+from products import products
+
+from products import products
+
+for product in products:
+    reviews = product['reviews']
+    
+    if len(reviews) == 0:
+        print(product['id'], 0)
+    else:
+        total = 0
+        
+        for review in reviews:
+            total += review['rating']
+        
+        avg = total / len(reviews)
+        print(product['id'], avg)
